3d3f8f1498ed44c5df2232c706f0f5edcf4208d3
apllication <apps.mav@outlook.com>
2026-09-23 12:46:14 -0300

    Adicionar configuracao inicial do Firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Configuração Web do Firebase. Substitua os valores pelo config do seu projeto.
+window.FONOApoioFirebaseConfig = {
+  apiKey: "COLOQUE_AQUI",
+  authDomain: "SEU-PROJETO.firebaseapp.com",
+  projectId: "SEU-PROJETO",
+  storageBucket: "SEU-PROJETO.firebasestorage.app",
+  messagingSenderId: "COLOQUE_AQUI",
+  appId: "COLOQUE_AQUI"
+};
